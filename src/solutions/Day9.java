d666874bb537fec70a883bd3c40282b406bdf6aa
Adam Barron <abarron@scottlogic.com>
2022-12-08 12:26:04 +0000

    Remove accidentally commited files, add day 9 template

---
+++ src/solutions/Day9.java
@@ -0,0 +1,22 @@
+package solutions;
+
+import utils.AdventOfCodePuzzle;
+
+import java.io.IOException;
+
+public class Day9 extends AdventOfCodePuzzle {
+
+    public Day9() throws IOException {
+        super(9);
+    }
+
+    @Override
+    public void part1() {
+
+    }
+
+    @Override
+    public void part2() {
+
+    }
+}
